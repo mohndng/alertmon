b732259faca11e84d5151d5df9432dedd5cac010
Mohn <93068579+mohndng@users.noreply.github.com>
2022-11-22 00:40:58 +0800

    Delete alertmon.js

--- alertmon.js
+++
@@ -1,7 +0,0 @@
-      alert("Hello 👋");
-      alert("Welcome😊");
-      alert("🙋️ I am Mon");
-      alert("It's been a while since I got interested in web developing. 💻");
-      alert("Particularly, front-end.");
-      alert("Anyways, this just a run test. 🏃");
-      alert("This is very simple implementatiom only. Don't expect too much. 😑")
